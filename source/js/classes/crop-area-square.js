f5eb88556b780e29e7e42ca98cf86d35a773f37e
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2015-10-22 13:56:27 +0200

    fix(forceAspectRatio) : handle resize with corners

--- source/js/classes/crop-area-square.js
+++ source/js/classes/crop-area-square.js
@@ -150,11 +150,13 @@ crop.factory('cropAreaSquare', ['cropArea', function(CropArea) {
             switch (this._resizeCtrlIsDragging) {
                 case 0: // Top Left
                     newNO.x = se.x - newSize;
-                        newNO.y = se.y - newSize;
-                    this.setSizeByCorners(newNO, {
-                        x: se.x,
-                        y: se.y
-                    });
+                    newNO.y = se.y - newSize;
+                    if(newNO.y > 0) {
+                        this.setSizeByCorners(newNO, {
+                            x: se.x,
+                            y: se.y
+                        });
+                    }
                     cursor = 'nwse-resize';
                     break;
                 case 1: // Top Right
@@ -167,13 +169,15 @@ crop.factory('cropAreaSquare', ['cropArea', function(CropArea) {
                         newNE.x = s.x + newSize;
                         newNE.y = se.y - newSize;
                     }
-                    this.setSizeByCorners({
-                        x: s.x,
-                        y: newNE.y
-                    }, {
-                        x: newNE.x,
-                        y: se.y
-                    });
+                    if(newNE.y > 0) {
+                        this.setSizeByCorners({
+                            x: s.x,
+                            y: newNE.y
+                        }, {
+                            x: newNE.x,
+                            y: se.y
+                        });
+                    }
                     cursor = 'nesw-resize';
                     break;
                 case 2: // Bottom Left
@@ -186,13 +190,15 @@ crop.factory('cropAreaSquare', ['cropArea', function(CropArea) {
                         newSO.x = se.x - newSize;
                         newSO.y = s.y + newSize;
                     }
-                    this.setSizeByCorners({
-                        x: newSO.x,
-                        y: s.y
-                    }, {
-                        x: se.x,
-                        y: newSO.y
-                    });
+                    if(newSO.y < this._ctx.canvas.height) {
+                        this.setSizeByCorners({
+                            x: newSO.x,
+                            y: s.y
+                        }, {
+                            x: se.x,
+                            y: newSO.y
+                        });
+                    }
                     cursor = 'nesw-resize';
                     break;
                 case 3: // Bottom Right
@@ -200,10 +206,12 @@ crop.factory('cropAreaSquare', ['cropArea', function(CropArea) {
                     newSE.x = s.x + newSize;
                     newSE.y = s.y + newSize;
 
-                    this.setSizeByCorners({
-                        x: s.x,
-                        y: s.y
-                    }, newSE);
+                    if(newSE.y < this._ctx.canvas.height) {
+                        this.setSizeByCorners({
+                            x: s.x,
+                            y: s.y
+                        }, newSE);
+                    }
                     cursor = 'nwse-resize';
                     break;
             }
@@ -237,7 +245,6 @@ crop.factory('cropAreaSquare', ['cropArea', function(CropArea) {
             }
         }
 
-        //this._dontDragOutside();
         angular.element(this._ctx.canvas).css({
             'cursor': cursor
         });
