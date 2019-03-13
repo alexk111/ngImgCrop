6051ae316048e1070fb9a50696ea90b6200e2908
Çağdaş Kurultay Kalkan <c.kurultay@orgino.com.tr>
2019-03-13 12:21:43 +0300

    add mouse wheel support

--- source/js/ng-img-crop.js
+++ source/js/ng-img-crop.js
@@ -1,6 +1,6 @@
 'use strict';
 
-crop.directive('imgCrop', ['$timeout', 'cropHost', 'cropPubSub', function($timeout, CropHost, CropPubSub) {
+crop.directive('imgCrop', ['$timeout', 'cropHost', 'cropPubSub', function ($timeout, CropHost, CropPubSub) {
   return {
     restrict: 'E',
     scope: {
@@ -13,42 +13,42 @@ crop.directive('imgCrop', ['$timeout', 'cropHost', 'cropPubSub', function($timeo
       resultImageSize: '=',
       resultImageFormat: '@',
       resultImageQuality: '=',
-
+      mouseWheelZoom: '=',
       onChange: '&',
       onLoadBegin: '&',
       onLoadDone: '&',
-      onLoadError: '&'
+      onLoadError: '&',
     },
     template: '<canvas></canvas>',
-    controller: ['$scope', function($scope) {
+    controller: ['$scope', function ($scope) {
       $scope.events = new CropPubSub();
     }],
-    link: function(scope, element/*, attrs*/) {
+    link: function (scope, element/*, attrs*/) {
       // Init Events Manager
       var events = scope.events;
 
       // Init Crop Host
-      var cropHost=new CropHost(element.find('canvas'), {}, events);
+      var cropHost = new CropHost(element.find('canvas'), {}, events);
 
       // Store Result Image to check if it's changed
       var storedResultImage;
 
-      var updateResultImage=function(scope) {
-        var resultImage=cropHost.getResultImageDataURI();
-        if(storedResultImage!==resultImage) {
-          storedResultImage=resultImage;
-          if(angular.isDefined(scope.resultImage)) {
-            scope.resultImage=resultImage;
+      var updateResultImage = function (scope) {
+        var resultImage = cropHost.getResultImageDataURI();
+        if (storedResultImage !== resultImage) {
+          storedResultImage = resultImage;
+          if (angular.isDefined(scope.resultImage)) {
+            scope.resultImage = resultImage;
           }
-          scope.onChange({$dataURI: scope.resultImage});
+          scope.onChange({ $dataURI: scope.resultImage });
         }
       };
 
       // Wrapper to safely exec functions within $apply on a running $digest cycle
-      var fnSafeApply=function(fn) {
-        return function(){
-          $timeout(function(){
-            scope.$apply(function(scope){
+      var fnSafeApply = function (fn) {
+        return function () {
+          $timeout(function () {
+            scope.$apply(function (scope) {
               fn(scope);
             });
           });
@@ -57,45 +57,49 @@ crop.directive('imgCrop', ['$timeout', 'cropHost', 'cropPubSub', function($timeo
 
       // Setup CropHost Event Handlers
       events
-        .on('load-start', fnSafeApply(function(scope){
-          scope.onLoadBegin({});
-        }))
-        .on('load-done', fnSafeApply(function(scope){
-          scope.onLoadDone({});
-        }))
-        .on('load-error', fnSafeApply(function(scope){
-          scope.onLoadError({});
-        }))
-        .on('area-move area-resize', fnSafeApply(function(scope){
-          if(!!scope.changeOnFly) {
-            updateResultImage(scope);
-          }
-        }))
-        .on('area-move-end area-resize-end image-updated', fnSafeApply(function(scope){
+      .on('load-start', fnSafeApply(function (scope) {
+        scope.onLoadBegin({});
+      }))
+      .on('load-done', fnSafeApply(function (scope) {
+        scope.onLoadDone({});
+      }))
+      .on('load-error', fnSafeApply(function (scope) {
+        scope.onLoadError({});
+      }))
+      .on('area-move area-resize', fnSafeApply(function (scope) {
+        if (!!scope.changeOnFly) {
           updateResultImage(scope);
-        }));
+        }
+      }))
+      .on('area-move-end area-resize-end image-updated', fnSafeApply(function (scope) {
+        updateResultImage(scope);
+      }));
 
       // Sync CropHost with Directive's options
-      scope.$watch('image',function(){
+      scope.$watch('image', function () {
         cropHost.setNewImageSource(scope.image);
       });
-      scope.$watch('areaType',function(){
+      scope.$watch('areaType', function () {
         cropHost.setAreaType(scope.areaType);
         updateResultImage(scope);
       });
-      scope.$watch('areaMinSize',function(){
+      scope.$watch('areaMinSize', function () {
         cropHost.setAreaMinSize(scope.areaMinSize);
         updateResultImage(scope);
       });
-      scope.$watch('resultImageSize',function(){
+      scope.$watch('mouseWheelZoom', function () {
+        cropHost.setMouseWheelZoom(scope.mouseWheelZoom);
+        updateResultImage(scope);
+      });
+      scope.$watch('resultImageSize', function () {
         cropHost.setResultImageSize(scope.resultImageSize);
         updateResultImage(scope);
       });
-      scope.$watch('resultImageFormat',function(){
+      scope.$watch('resultImageFormat', function () {
         cropHost.setResultImageFormat(scope.resultImageFormat);
         updateResultImage(scope);
       });
-      scope.$watch('resultImageQuality',function(){
+      scope.$watch('resultImageQuality', function () {
         cropHost.setResultImageQuality(scope.resultImageQuality);
         updateResultImage(scope);
       });
@@ -106,16 +110,16 @@ crop.directive('imgCrop', ['$timeout', 'cropHost', 'cropPubSub', function($timeo
           return [element[0].clientWidth, element[0].clientHeight];
         },
         function (value) {
-          cropHost.setMaxDimensions(value[0],value[1]);
+          cropHost.setMaxDimensions(value[0], value[1]);
           updateResultImage(scope);
         },
-        true
+        true,
       );
 
       // Destroy CropHost Instance when the directive is destroying
-      scope.$on('$destroy', function(){
-          cropHost.destroy();
+      scope.$on('$destroy', function () {
+        cropHost.destroy();
       });
-    }
+    },
   };
 }]);
